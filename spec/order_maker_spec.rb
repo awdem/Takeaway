fe460b8903f3bda26e56638c98fb56d408c3111e
awdem <wdemontmollin@gmail.com>
2023-04-21 19:53:36 +0100

    turned test two into mock test

--- spec/order_maker_spec.rb
+++ spec/order_maker_spec.rb
@@ -17,12 +17,11 @@ RSpec.describe OrderMaker do
   end
 
   it "fails if trying to select a dish that is not on the menu" do
-    customer = Customer.new("010000000001")
-    dish1 = Dish.new("name1", "price1")
-    dish2 = Dish.new("name2", "price2")
-    menu = Menu.new
-    new_order = Order.new(customer)
-    menu.add(dish1)
+    customer = double :customer
+    dish1 = double :dish
+    dish2 = double :dish
+    menu = double :dish, list: [dish1]
+    new_order = double :order, list: []
     order_in_progress = OrderMaker.new(menu, new_order, customer)
     expect{order_in_progress.select(dish2)}.to raise_error "Not on the menu!"      
   end
