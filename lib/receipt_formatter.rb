ab78fd3d965b5be20e9803ad6bea7858f20cb8b5
awdem <wdemontmollin@gmail.com>
2023-04-21 20:20:56 +0100

    passed receipt formatter integration/unit test

--- lib/receipt_formatter.rb
+++ lib/receipt_formatter.rb
@@ -1,10 +1,21 @@
 class ReceiptFormatter
   def initialize(order_in_progress) # order_in_progress is an instance of the OrderMaker class
-
+    @order_in_progress = order_in_progress
   end
 
   def format
-    # returns formatted receipt as a string
+    itemized_list = @order_in_progress.check.list.map do |dish|
+      "- #{dish.name}, price: #{dish.price}"
+    end.join("\n")
+    itemized_list + "\ntotal price: #{self.sum}"
+  end
+
+  private
+
+  def sum
+    @order_in_progress.check.list.sum do |dish|
+     dish.price.to_i 
+    end 
   end
  
 end
