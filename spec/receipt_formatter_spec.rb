5d2462806756b5c7425b429de698f00da2cde87c
awdem <wdemontmollin@gmail.com>
2023-04-23 16:50:09 +0100

    mocked receipt formatter unit tests

--- spec/receipt_formatter_spec.rb
+++ spec/receipt_formatter_spec.rb
@@ -3,32 +3,20 @@ require "receipt_formatter"
 RSpec.describe ReceiptFormatter do
   context "given a menu with some dishes listed and an order in progress" do
     it "allows customer to see an itemised receipt of the order in progress with sum of prices" do
-      customer = Customer.new("010000000001")
-      dish1 = Dish.new("name1", "1")
-      dish2 = Dish.new("name2", "2")
-      menu = Menu.new
-      new_order = Order.new(customer)
-      menu.add(dish1)
-      menu.add(dish2)
-      order_in_progress = OrderMaker.new(menu, new_order, customer)
-      order_in_progress.select(dish1)
-      order_in_progress.select(dish2)
+      dish1 = double :dish, name: "name1", price: "1"
+      dish2 = double :dish, name: "name2", price: "2"
+      new_order = double :order, list: [dish1, dish2]
+      order_in_progress = double :order_maker, check: new_order
       receipt = ReceiptFormatter.new(order_in_progress).format
       
       expect(receipt).to eq "- name1, price: 1\n- name2, price: 2\ntotal price: 3.0"
     end
 
     it "sums prices that are floats on receipt" do
-      customer = Customer.new("010000000001")
-      dish1 = Dish.new("name1", "1.99")
-      dish2 = Dish.new("name2", "2.99")
-      menu = Menu.new
-      new_order = Order.new(customer)
-      menu.add(dish1)
-      menu.add(dish2)
-      order_in_progress = OrderMaker.new(menu, new_order, customer)
-      order_in_progress.select(dish1)
-      order_in_progress.select(dish2)
+      dish1 = double :dish, name: "name1", price: "1.99"
+      dish2 = double :dish, name: "name2", price: "2.99"
+      new_order = double :order, list: [dish1, dish2]
+      order_in_progress = double :order_maker, check: new_order
       receipt = ReceiptFormatter.new(order_in_progress).format
       
       expect(receipt).to eq "- name1, price: 1.99\n- name2, price: 2.99\ntotal price: 4.98"
